03dacfb9b3e601e9204bf7ab689dc5c3dc492104
Rajat083 <155555359+Rajat083@users.noreply.github.com>
2025-04-11 19:38:27 +0530

    Update settings.py

--- gamify/settings.py
+++ gamify/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'gamify.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'ARpNtmRuqgRIlAuvVWiREphffmpUPOMM',
-        'HOST': 'yamabiko.proxy.rlwy.net',  
-        'PORT': '35044',         
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
